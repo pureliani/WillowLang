85bee2e7a99d2537a6efe9839b9ea19072fbb884
Gabriel Pureliani <gafureliani@gmail.com>
2025-10-21 22:44:52 +0400

    refactor: main.rs

--- src/main.rs
+++ src/main.rs
@@ -1,27 +1,17 @@
-use std::fs;
+use std::{fs, path::PathBuf};
 
 use codespan_reporting::files::SimpleFiles;
-use willow::{
-    compile::{compile_file, string_interner::StringInterner},
-    hir::ProgramBuilder,
-};
+use willow::{compile::Compiler, hir::ProgramBuilder};
 
 fn main() {
     let file_path = match std::env::args().nth(1) {
         Some(path) => path,
         None => {
-            eprintln!("{}", "\nExpected file path to the program entry\n");
+            eprintln!("\nExpected file path to the program entry\n");
             return;
         }
     };
 
-    let mut string_interner = StringInterner::new();
-    let program_builder = ProgramBuilder::new(string_interner);
-    let mut files = SimpleFiles::new();
-
-    compile_file(&file_path, &source_code, &mut string_interner, &mut files);
-
-    // let mut coll = string_interner.forward.into_iter().collect::<Vec<_>>();
-    // coll.sort_by(|a, b| a.1.cmp(&b.1));
-    // coll.iter().for_each(|(name, id)| println!("{}: {}", id, name));
+    let mut compiler = Compiler::new();
+    compiler.compile(PathBuf::from(file_path));
 }
