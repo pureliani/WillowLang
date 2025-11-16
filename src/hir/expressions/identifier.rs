2a08be7702fa0952f4b660d3ccbed1dbf1e8046f
Gabriel Pureliani <gafureliani@gmail.com>
2025-11-16 23:49:39 +0400

    wip: closures

--- src/hir/expressions/identifier.rs
+++ src/hir/expressions/identifier.rs
@@ -22,7 +22,40 @@ impl FunctionBuilder {
                             Value::Use(self.emit_load(ctx, stack_ptr))
                         }
                         VarStorage::Captured => {
-                            todo!("Implement access for a captured variable");
+                            let env_param_name =
+                                ctx.program_builder.string_interner.intern("__env_ptr");
+
+                            let env_param_decl = ctx.module_builder.scope_lookup(env_param_name)
+                                 .expect("INTERNAL COMPILER ERROR: In a closure context, but the '__env_ptr' parameter was not found in scope.");
+
+                            let env_param_stack_ptr = if let CheckedDeclaration::Var(
+                                var_decl,
+                            ) = env_param_decl
+                            {
+                                if let VarStorage::Stack(ptr) = var_decl.storage {
+                                    ptr
+                                } else {
+                                    panic!("INTERNAL COMPILER ERROR: Environment pointer parameter is not on the stack.");
+                                }
+                            } else {
+                                panic!("INTERNAL COMPILER ERROR: Environment parameter declaration is not a variable.");
+                            };
+
+                            let env_ptr_id = self.emit_load(ctx, env_param_stack_ptr);
+
+                            let field_ptr_id = match self
+                                .emit_get_field_ptr(ctx, env_ptr_id, identifier)
+                            {
+                                Ok(ptr) => ptr,
+                                Err(e) => {
+                                    // This should ideally never happen if capture analysis was correct
+                                    return Value::Use(
+                                        self.report_error_and_get_poison(ctx, e),
+                                    );
+                                }
+                            };
+
+                            Value::Use(self.emit_load(ctx, field_ptr_id))
                         }
                     }
                 }
