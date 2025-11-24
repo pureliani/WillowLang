9406db8524a3353ce596b6cd2b46978e3951706e
Gabriel Pureliani <gafureliani@gmail.com>
2025-11-24 22:57:06 +0400

    fix: tag hir builder

--- src/hir/expressions/tag.rs
+++ src/hir/expressions/tag.rs
@@ -2,9 +2,13 @@ use crate::{
     ast::{expr::Expr, IdentifierNode, Span},
     hir::{
         cfg::Value,
-        types::{checked_declaration::CheckedTagType, checked_type::Type},
+        types::{
+            checked_declaration::TagType,
+            checked_type::{StructKind, Type},
+        },
         FunctionBuilder, HIRContext,
     },
+    tokenize::NumberKind,
 };
 
 impl FunctionBuilder {
@@ -15,25 +19,49 @@ impl FunctionBuilder {
         value: Option<Box<Expr>>,
         span: Span,
     ) -> Value {
+        let identifier_value = ctx.program_builder.common_identifiers.value;
+        let identifier_id = ctx.program_builder.common_identifiers.id;
+
         let tag_id = ctx.program_builder.tag_interner.intern(&name.name);
+
         let inner_value = value.map(|v| Box::new(self.build_expr(ctx, *v)));
+
         let inner_value_type = inner_value
             .as_ref()
             .map(|v| Box::new(ctx.program_builder.get_value_type(v)));
 
-        let checked_type = Type {
-            kind: TypeKind::Tag(CheckedTagType {
-                span,
-                identifier: name,
-                value_type: inner_value_type,
-            }),
+        let checked_type = Type::Struct(StructKind::Tag(TagType {
+            span,
+            id: tag_id,
+            value_type: inner_value_type,
+        }));
+
+        let tag_ptr = self.emit_stack_alloc(ctx, checked_type.clone(), 1);
+
+        let id_field_node = IdentifierNode {
+            name: identifier_id,
             span,
         };
 
-        Value::Tag {
-            tag_id,
-            value: inner_value,
-            ty: checked_type,
+        let id_ptr = self
+            .emit_get_field_ptr(ctx, tag_ptr, id_field_node)
+            .expect("INTERNAL COMPILER ERROR: StructKind::Tag missing 'id' field");
+
+        self.emit_store(ctx, id_ptr, Value::NumberLiteral(NumberKind::U16(tag_id.0)));
+
+        if let Some(v) = inner_value {
+            let val_field_node = IdentifierNode {
+                name: identifier_value,
+                span,
+            };
+
+            let value_ptr = self
+                .emit_get_field_ptr(ctx, tag_ptr, val_field_node)
+                .expect("INTERNAL COMPILER ERROR: StructKind::Tag missing 'value' field");
+
+            self.emit_store(ctx, value_ptr, *v);
         }
+
+        Value::Use(tag_ptr)
     }
 }
