10551e2cdc022a79c47f5cff2d0efb08e9310724
Gabriel Pureliani <gafureliani@gmail.com>
2025-12-04 00:22:00 +0400

    refactor: and expression

--- src/hir/expressions/and.rs
+++ src/hir/expressions/and.rs
@@ -2,6 +2,7 @@ use crate::{
     ast::expr::Expr,
     hir::{
         cfg::{Terminator, Value},
+        types::checked_type::Type,
         FunctionBuilder, HIRContext,
     },
 };
@@ -16,33 +17,32 @@ impl FunctionBuilder {
         let right_entry_block_id = self.new_basic_block();
         let merge_block_id = self.new_basic_block();
 
+        let result_param = self.append_block_param(ctx, merge_block_id, Type::Bool);
+
         let left_value = self.build_expr(ctx, *left);
-        let left_exit_block_id = self.current_block_id;
 
         self.set_basic_block_terminator(Terminator::CondJump {
             condition: left_value,
             true_target: right_entry_block_id,
+            true_args: vec![],
             false_target: merge_block_id,
+            false_args: vec![Value::BoolLiteral(false)],
         });
+
+        self.seal_block(ctx, right_entry_block_id);
 
         self.use_basic_block(right_entry_block_id);
         let right_value = self.build_expr(ctx, *right);
-        let right_exit_block_id = self.current_block_id;
+
         self.set_basic_block_terminator(Terminator::Jump {
             target: merge_block_id,
+            args: vec![right_value],
         });
 
+        self.seal_block(ctx, merge_block_id);
+
         self.use_basic_block(merge_block_id);
-        let phi_sources = vec![
-            (left_exit_block_id, Value::BoolLiteral(false)),
-            (right_exit_block_id, right_value),
-        ];
 
-        let phi_destination = match self.emit_phi(ctx, phi_sources) {
-            Ok(phi_destination) => phi_destination,
-            Err(err) => self.report_error_and_get_poison(ctx, err),
-        };
-
-        Value::Use(phi_destination)
+        Value::Use(result_param)
     }
 }
